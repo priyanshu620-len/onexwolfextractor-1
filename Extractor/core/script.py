35eb821b0730f0fad02415374f5c93cb7f41b152
priyanshu620-len <dwivedisantosh317@gmail.com>
2026-02-28 22:07:59 +0530

    Update script.py

--- Extractor/core/script.py
+++ Extractor/core/script.py
@@ -1,38 +1,109 @@
 import config
+from config import ADMIN_BOT_USERNAME
 
-# Safe way to get username from config
-ADMIN_BOT_USERNAME = getattr(config, "ADMIN_BOT_USERNAME", "YourAdminUsername")
 
-class script(object):
-    # We use a regular string here so .format() in start.py works correctly
-    START_TXT = f"""
-<b><u>👋 Hello {{}} , I'm Txt Extractor Bot at your service. 🤖</u>
+# ------------------------------------------------------------ #
 
-To start extracting URLs, simply send /app. 📲
+START_TXT = """
+**ʜᴇʟʟᴏ** {},
 
-<blockquote><i>🔓 I support over 100+ apps for seamless URL extraction.
-📥 After extracting the URLs, you can easily download the videos/pdfs using any of my Uploader Bot.⏬</blockquote></i>
+**<blockquote>ɪ'ᴍ ᴀᴅᴇᴘᴛ ᴀᴛ ᴇxᴛʀᴀᴄᴛɪɴɢ ᴏɴʟɪɴᴇ ᴄᴏᴜʀsᴇs ᴡɪᴛʜ ᴄᴜᴛᴛɪɴɢ-ᴇᴅɢᴇ ᴛᴇᴄʜɴᴏʟᴏɢʏ. ɪ'ᴠᴇ ᴍᴀɴᴀɢᴇᴅ ᴛᴏ ɢᴀᴛʜᴇʀ ʟɪɴᴋs ғᴏʀ ᴀʟᴍᴏsᴛ ᴀʟʟ ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴜʀsᴇs, ᴍᴀᴋɪɴɢ ᴛʜᴇ ᴘʀᴏᴄᴇss ᴇғғᴏʀᴛʟᴇss ᴀɴᴅ ᴇғғɪᴄɪᴇɴᴛ.</blockquote>**
 
-<u>For any queries, contact <a href="https://t.me/{ADMIN_BOT_USERNAME}">Admin</a></u></b>
+**<blockquote>🌿 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href="@Saini_extractor_bot">Aᴅᴍɪɴ 🫠</a></blockquote>**
 """
 
-    # These are needed for your button callbacks
-    MODES_TXT = "<b>Please select the extraction mode you want to use below:</b>"
+FORCE_MSG = """
+**ʜᴇʏ** {}
+
+**<blockquote>ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ ʏᴇᴛ, ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴜsᴇ ᴍᴇ ᴛʜᴇɴ ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ ᴀɴᴅ sᴛᴀʀᴛ ᴍᴇ ᴀɢᴀɪɴ !</blockquote>**
+
+**<blockquote>🌿 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href="@Saini_extractor_bot">Aᴅᴍɪɴ 🫠</a></blockquote>**
+"""
+
+MODES_TXT = """
+**<blockquote>ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛʜᴇ ᴍᴏᴅᴇ sᴇᴄᴛɪᴏɴ. ʜᴇʀᴇ, ʏᴏᴜ ᴄᴀɴ ᴄʜᴏᴏsᴇ ʙᴇᴛᴡᴇᴇɴ ᴛᴡᴏ ᴍᴏᴅᴇs: ʟᴏɢɪɴ ᴍᴏᴅᴇ ᴏʀ ᴡɪᴛʜᴏᴜᴛ ʟᴏɢɪɴ ᴍᴏᴅᴇ.</blockquote>**
+"""
+
+CUSTOM_TXT = """
+⌬ **﹝𝐖 𝐈 𝐓 𝐇 𝐎 𝐔 𝐓 𝐋 𝐎 𝐆 𝐈 𝐍﹞**
+
+**<blockquote>ɪɴ ᴡɪᴛʜᴏᴜᴛ ʟᴏɢɪɴ ᴍᴏᴅᴇ, ʏᴏᴜ ᴄᴀɴ ᴇxᴛʀᴀᴄᴛ ᴀɴʏ ᴄᴏᴜʀsᴇ ғʀᴏᴍ ᴀɴʏ ᴀᴘᴘ's ʟɪɴᴋ, ʙᴜᴛ ʏᴏᴜ'ʟʟ ɴᴇᴇᴅ ᴀɴ ᴘʀᴇᴍɪᴜᴍ ғᴏʀ ᴛʜᴀᴛ.</blockquote>**
+
+"""
+
+MANUAL_TXT = """
+⌬ **﹝𝐋 𝐎 𝐆 𝐈 𝐍﹞**
+
+**<blockquote>ɪɴ ʟᴏɢɪɴ ᴍᴏᴅᴇ, ʏᴏᴜ ʜᴀᴠᴇ ʙᴇᴇɴ ɢɪᴠᴇɴ ʙᴜᴛᴛᴏɴs. ᴄʜᴏᴏsᴇ ᴛʜᴇ ᴏɴᴇ ᴡɪᴛʜ ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅ ᴛᴏ ᴇxᴛʀᴀᴄᴛ ᴛʜᴇ ᴄᴏᴜʀsᴇ ʏᴏᴜ ᴡᴀɴᴛ.</blockquote>**
+"""
+
+
+PLANS_TXT = """
+⌬ **﹝𝐏 𝐑 𝐄 𝐌 𝐈 𝐔 𝐌﹞**
+
+<b><blockquote>ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛʜᴇ ᴇxᴄʟᴜsɪᴠᴇ ᴘʀᴇᴍɪᴜᴍ sᴇᴄᴛɪᴏɴ, ᴡʜᴇʀᴇ ʏᴏᴜ'ʟʟ ғɪɴᴅ ᴄᴜʀᴀᴛᴇᴅ ɪɴsɪɢʜᴛs ᴀɴᴅ ᴇxᴘᴇʀɪᴇɴᴄᴇs ᴛᴀɪʟᴏʀᴇᴅ ᴛᴏ ᴛʜᴏsᴇ sᴇᴇᴋɪɴɢ ᴛᴏᴘ-ᴛɪᴇʀ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀɴᴅ sᴇʀᴠɪᴄᴇs.</blockquote>
+
+<blockquote>‼️ ᴀғᴛᴇʀ sᴇɴᴅɪɴɢ ᴀ sᴄʀᴇᴇɴsʜᴏᴛ ᴘʟᴇᴀsᴇ ɢɪᴠᴇ ᴜs sᴏᴍᴇ ᴛɪᴍᴇ ᴛᴏ ᴀᴅᴅ ʏᴏᴜ ɪɴ ᴛʜᴇ ᴘʀᴇᴍɪᴜᴍ ʟɪsᴛ.</blockquote></b>
+**<blockquote>🌿 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href="@Saini_extractor_bot">Aᴅᴍɪɴ 🫠</a></blockquote>**
+"""
+
     
-    CUSTOM_TXT = "<b>Select a direct extraction method (No login required):</b>"
+FREE_TXT = """
+⌬ **﹝𝐅 𝐑 𝐄 𝐄﹞**
+<b>
+🏆 <u>ꜰʀᴇᴇ ᴛʀɪᴀʟ</u> 🏆
+❗ ᴏɴʟʏ ꜰᴏʀ 5 ᴍɪɴᴜᴛᴇꜱ
+ 
+○ ʙʏᴘᴀss ᴀʟʟ ᴄᴏᴜʀsᴇ
+○ ғᴀsᴛ ɴᴏᴅᴇ 
+</b>"""
 
-    MANUAL_TXT = f"""
-<b><i>👋 Hey there! I'm your Txt Extractor bot!🤖</i>
+BRONZE_TXT = """
+⌬ **﹝𝐁 𝐑 𝐎 𝐍 𝐙 𝐄﹞**
+<b>
+🥉 <u>ʙʀᴏɴᴄᴇ ᴘʟᴀɴ</u>
+⏰ 7 ᴅᴀʏꜱ
+💸 ᴘʟᴀɴ ᴘʀɪᴄᴇ ➛ 300₹
+</b>"""
 
-<blockquote>🚨 If you encounter any issues or can't extract any text file, just contact your 
-<a href="https://t.me/{ADMIN_BOT_USERNAME}">Admin</a>.
+SILVER_TXT = """
+⌬ **﹝𝐒 𝐈 𝐋 𝐕 𝐄 𝐑﹞**
+<b> 
+🥈 <u>ꜱɪʟᴠᴇʀ ᴘʟᴀɴ</u>
+⏰ 15 ᴅᴀʏꜱ 
+💸 ᴘʟᴀɴ ᴘʀɪᴄᴇ ➛ 500₹
+</b>"""
 
-💬 Got an app you'd like to add? Feel free to reach out anytime!</blockquote>
+GOLD_TXT = """
+⌬ **﹝𝐆 𝐎 𝐋 𝐃﹞**  
+<b>
+🥇 <u>ɢᴏʟᴅ ᴘʟᴀɴ</u>
+⏰ 30 ᴅᴀʏꜱ 
+💸 ᴘʟᴀɴ ᴘʀɪᴄᴇ ➛ 800₹
+</b>"""
 
-<i>👇 Choose an option below and let's get started!</i></b>
-"""
 
-    UPGRADE = """
-<b><u>Hey👋, Choose Your Plan Below:-</u></b>
-<i>Contact Admin for premium features.</i>
-"""
+OTHER_TXT = """
+⌬ **﹝𝐏 𝐋 𝐀 𝐍 𝐒﹞** 
+<b>
+🎁 <u>ᴏᴛʜᴇʀ ᴘʟᴀɴ</u>
+⏰ ᴄᴜꜱᴛᴏᴍɪꜱᴇᴅ ᴅᴀʏꜱ
+💸 ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ᴅᴀʏꜱ ʏᴏᴜ ᴄʜᴏᴏꜱᴇ
+
+👨‍💻 ᴄᴏɴᴛᴀᴄᴛ ᴛʜᴇ ᴏᴡɴᴇʀ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ᴏᴛʜᴇʀ ᴘʟᴀɴ.
+</b>"""
+
+
+PAYMENT_TXT = """<b>
+⚜️ ᴘᴀʏ ᴀᴍᴍᴏᴜɴᴛ ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ʏᴏᴜʀ ᴘʟᴀɴ ᴀɴᴅ ᴇɴᴊᴏʏ ᴘʀᴇᴍɪᴜᴍ ᴍᴇᴍʙᴇʀꜱʜɪᴘ !
+
+📸 ǫʀ ᴄᴏᴅᴇ - <a href='https://envs.sh/24T.jpg'>ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ꜱᴄᴀɴ</a>
+
+‼️ ᴍᴜsᴛ sᴇɴᴅ sᴄʀᴇᴇɴsʜᴏᴛ ᴀғᴛᴇʀ ᴘᴀʏᴍᴇɴᴛ.
+</b>"""
+
+
+
+
+
+
